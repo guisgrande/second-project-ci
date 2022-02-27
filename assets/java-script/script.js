c0bac75f42d1e6d43a0a3eabbc8940cbb4f7e447
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-02-27 16:33:13 +0000

    Add function names and comments

---
+++ assets/java-script/script.js
@@ -0,0 +1,50 @@
+// converter.html functions
+
+/**
+ * Receives the value of metric 1 and returns the conversation for metric 2.
+ */
+function convertMetrics() {
+
+}
+
+/**
+ * Exchanges metric 1 for metric 2.
+ */
+function swapMetrics() {
+
+}
+
+/**
+ * Clear all the values.
+ */
+function clearMetrics() {
+
+}
+
+// quiz-game.html functions
+
+/**
+ * Start or restart the game, show the first question,
+ * show 3 answers options, resets the counters to zero,
+ * and starts the timer in 30 seconds (countdown). 
+ */
+function startRestart() {
+    let gameStart = document.getElementById('start-restart');
+}
+
+/**
+ * Stop the game and shows the answers report.
+ */
+function stopReport() {
+
+}
+
+/**
+ * Receives the player's answer,
+ * returns whether it is correct or incorrect and increments the score,
+ * shows the next question and answers,
+ * restarts the counter at 30 seconds.
+ */
+function answerFeedback() {
+
+}
