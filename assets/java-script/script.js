4e37c0e74835105daee312c2632f7c9f96d08596
Guilherme Souza Grande <gsouzagrande@gmail.com>
2022-02-28 09:01:48 +0000

    Add startRestar and timer basic functions code

--- assets/java-script/script.js
+++ assets/java-script/script.js
@@ -23,13 +23,42 @@ function clearMetrics() {
 
 // quiz-game.html functions
 
+// Timer function
+function timerStart() {
+let timerSeconds = 30;
+let timerCountdown = document.getElementById('timer');
+timerCountdown.innerHTML = `Timer: ${timerSeconds}`;
+let timerDecres = setInterval (()=>{
+    timerSeconds--;
+    timerCountdown.innerHTML = `Timer: ${timerSeconds}`;
+    if (timerSeconds <= 0 || timerSeconds < 1) {
+        clearInterval(timerDecres)
+        // call new question
+        // add incorrect answer
+        // start timer again         
+    }
+}, 1000);
+}
+
 /**
  * Start or restart the game, show the first question,
  * show 3 answers options, resets the counters to zero,
  * and starts the timer in 30 seconds (countdown). 
  */
 function startRestart() {
-    let gameStart = document.getElementById('start-restart');
+    // Remove rules
+    let cleanRules = document.getElementById('rules-box');
+    cleanRules.parentNode.removeChild(cleanRules);
+
+    // Reset the counters to zero
+    let correctReset = document.getElementById('correct-score').innerHTML = "Correct Answers: 00";
+    let incorrectReset = document.getElementById('incorrect-score').innerHTML = "Incorrect Answers: 00";
+
+    // Start the timer
+    timerStart();
+    // Create a question
+    
+    // Create the anwers
 }
 
 /**
